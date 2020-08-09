48e52bbed2b593c33ff86850042eb11a703ede43
Falk <julien.delusseau@gmail.com>
2020-08-09 10:51:11 +0200

    Added mongoose and create User model

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 const authRoutes = require('./routes/authRoutes')
 require('./services/passport')
+require('./models/User')
 
 const app = express()
 authRoutes(app)
 
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log('Server started...')
-})
+app.listen(PORT)
