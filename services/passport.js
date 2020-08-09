f83e17c83415ee755e82f7a0d356f74c7a481ec5
Falk <julien.delusseau@gmail.com>
2020-08-09 11:40:01 +0200

    Added the logic to save an user in the database

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const keys = require('../config/keys')
+const mongoose = require('mongoose')
+
+const User = mongoose.model('users')
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +13,14 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, cb) => {
-      console.log('accessToken: ', accessToken)
-      console.log('refreshToken: ', refreshToken)
-      console.log('profile: ', profile)
+      try {
+        User.findOne({ googleId: profile.id }).then(existingUser => {
+          if (existingUser) return
+          new User({ googleId: profile.id }).save()
+        })
+      } catch (err) {
+        throw new Error('Can not create a new user. ', err)
+      }
     }
   )
 )
